cd494fa35e54a78848f72d217c4ef3bf08a2a5f7
aman.mian <aman.mian@DESKTOP-Q8L0J26>
2018-02-05 19:03:51 +0530

    First commit

---
+++ WebApplication4/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebApplication4.Dtos;
+using WebApplication4.Entities;
+
+namespace WebApplication4.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
---
+++ WebApplication4/Helpers/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Entities;
+
+namespace WebApplication4.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
